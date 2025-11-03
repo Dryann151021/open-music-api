aaf2ce649bf38dc303e843938fa54c0a1b6cb206
Adriyan <adrianadrian.aa613@gmail.com>
2025-11-03 23:10:27 +0700

    feat auth strategy: menambahkan authentications strategy dengan plugin Hapi dan diterapkan pada route albums serta songs

--- src/api/albums/routes.js
+++ src/api/albums/routes.js
@@ -3,21 +3,33 @@ const routes = (handler) => [
     method: 'POST',
     path: '/albums',
     handler: handler.postAlbumHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/albums/{id}',
     handler: handler.getAlbumDetailHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'PUT',
     path: '/albums/{id}',
     handler: handler.putAlbumDetailHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'DELETE',
     path: '/albums/{id}',
     handler: handler.deleteAlbumDetailHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
 ];
 
--- src/api/songs/routes.js
+++ src/api/songs/routes.js
@@ -3,26 +3,41 @@ const routes = (handler) => [
     method: 'POST',
     path: '/songs',
     handler: handler.postSongHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/songs',
     handler: handler.getSongsHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'GET',
     path: '/songs/{id}',
     handler: handler.getSongDetailHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'PUT',
     path: '/songs/{id}',
     handler: handler.putSongDetailHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
   {
     method: 'DELETE',
     path: '/songs/{id}',
     handler: handler.deleteSongDetailHandler,
+    options: {
+      auth: 'openmusic_jwt',
+    },
   },
 ];
 
